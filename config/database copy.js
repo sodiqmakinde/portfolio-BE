064040b732c57f7320532865dd6cc50aa8767372
Makinde Sodiq <87417063+sodiqmakinde@users.noreply.github.com>
2025-05-11 21:55:16 +0100

    overide

--- config/database copy.js
+++ config/database copy.js
@@ -5,21 +5,21 @@ module.exports = ({ env }) => {
 
   const connections = {
     mysql: {
-      connection: {
-        host: env('DATABASE_HOST', 'db.vnjkaruxsilaplycvwxb.supabase.co'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'postgres'),
-        user: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', '$$Billions009'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
-        },
-      },
+      // connection: {
+      //   host: env('DATABASE_HOST', 'db.vnjkaruxsilaplycvwxb.supabase.co'),
+      //   port: env.int('DATABASE_PORT', 5432),
+      //   database: env('DATABASE_NAME', 'postgres'),
+      //   user: env('DATABASE_USERNAME', 'postgres'),
+      //   password: env('DATABASE_PASSWORD', '$$Billions009'),
+      //   ssl: env.bool('DATABASE_SSL', false) && {
+      //     key: env('DATABASE_SSL_KEY', undefined),
+      //     cert: env('DATABASE_SSL_CERT', undefined),
+      //     ca: env('DATABASE_SSL_CA', undefined),
+      //     capath: env('DATABASE_SSL_CAPATH', undefined),
+      //     cipher: env('DATABASE_SSL_CIPHER', undefined),
+      //     rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
+      //   },
+      // },
       // connection: {
       //   host: env('DATABASE_HOST', 'db.vnjkaruxsilaplycvwxb.supabase.co'),
       //   port: env.int('DATABASE_PORT', 3306),
