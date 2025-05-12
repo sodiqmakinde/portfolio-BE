eddf6ab1e1e387091281b81d697bd7996281777d
Makinde Sodiq <87417063+sodiqmakinde@users.noreply.github.com>
2025-05-12 07:57:41 +0100

    Postgress

--- config/database.js
+++ config/database.js
@@ -1,38 +1,18 @@
-'use strict';
-const path = require('path');
-
-module.exports = ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'postgres');
-
-  const ssl =
-    env.bool('DATABASE_SSL', false) && {
-      rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
-    };
-
-  const connections = {
-    postgres: {
+    // strapi-api/config/database.js
+    module.exports = ({ env }) => ({
       connection: {
-        connectionString: env('DATABASE_URL'),
-        ssl,
+        client: 'postgres',
+        connection: {
+          host: env('DATABASE_HOST'),
+          port: env.int('DATABASE_PORT', 5432),
+          database: env('DATABASE_NAME'),
+          user: env('DATABASE_USERNAME'),
+          password: env('DATABASE_PASSWORD'),
+          schema: env('DATABASE_SCHEMA', 'public'), // Not required
+          ssl: {
+            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+          },
+        },
+        debug: false,
       },
-      pool: {
-        min: env.int('DATABASE_POOL_MIN', 2),
-        max: env.int('DATABASE_POOL_MAX', 10),
-      },
-    },
-    sqlite: {
-      connection: {
-        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-      },
-      useNullAsDefault: true,
-    },
-  };
-
-  return {
-    connection: {
-      client,
-      ...connections[client],
-      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
-    },
-  };
-};
+    });
